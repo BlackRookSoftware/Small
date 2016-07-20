84f2c2f75379179acfcff7624369369cfdf9ed9c
mtrop <mtropiano@gmail.com>
2016-07-20 18:44:33 -0400

    Added URITrie stuff

--- src/com/blackrook/j2ee/small/struct/URITrie.java
+++ src/com/blackrook/j2ee/small/struct/URITrie.java
@@ -1,5 +1,11 @@
 package com.blackrook.j2ee.small.struct;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.blackrook.commons.list.SortedList;
+import com.blackrook.j2ee.small.descriptor.ControllerEntryMethod;
+
 /**
  * A trie that organizes mapping URI patterns to ControllerHandlers.
  * @author Matthew Tropiano
@@ -8,17 +14,67 @@ public class URITrie
 {
 	// TODO: Finish
 	
+	private enum NodeType
+	{
+		ROOT,
+		MATCH,
+		PATHVARIABLE;
+	}
+
+	/**
+	 * A single node.
+	 */
+	private static class Node implements Comparable<Node>
+	{
+		NodeType type;
+		String token;
+		Pattern pattern;
+		boolean remainder;
+		ControllerEntryMethod entryMethod;
+		SortedList<Node> edges;
+		
+		private Node(NodeType type, String token, Pattern pattern, boolean remainder)
+		{
+			this.type = type;
+			this.token = token;
+			this.pattern = pattern;
+			this.remainder = remainder;
+			this.entryMethod = null;
+			this.edges = new SortedList<>();
+		}
+		
+		static Node createRoot()
+		{
+			return new Node(NodeType.ROOT, null, null, false);
+		}
+		
+		static Node createMatchNode(String token, boolean remainder)
+		{
+			return new Node(NodeType.MATCH, token, null, remainder);
+		}
+		
+		static Node createVariableNode(String token, Pattern pattern, boolean remainder)
+		{
+			return new Node(NodeType.PATHVARIABLE, token, pattern, remainder);
+		}
+
+		@Override
+		public int compareTo(Node n)
+		{
+			return type != n.type 
+					? type.ordinal() - n.type.ordinal() 
+					: !token.equals(n.token) 
+						? token.compareTo(n.token) 
+						: 0;
+		}
+		
+	}
 	
 	/*
 		URITrie
 			root : URITrie.Node
 			resolve(String uri) : URITrie.Result
 		
-		.NodeType
-			ROOT
-			MATCH
-			PATHVARIABLE
-			
 		.Node -> Comparable<Node>
 			type : NodeType
 			token : String			// MATCH key or PATH_VARIABLE name
@@ -43,5 +99,6 @@ public class URITrie
 			entryPoint : ControllerMethodDescriptor
 		
 	 */
+	
 	
 }
