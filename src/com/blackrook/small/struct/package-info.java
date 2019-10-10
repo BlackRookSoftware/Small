c32cce751a480fd3d050a089d48c8e2d35490afc
mtrop <mtropiano@gmail.com>
2019-10-10 00:39:21 -0400

    Changed a package notice.

--- src/com/blackrook/small/struct/package-info.java
+++ src/com/blackrook/small/struct/package-info.java
@@ -1,2 +1,5 @@
-/** This package contains useful data structures. */
+/**
+ * Additional helper data structures and methods.
+ * <p><b>NOTICE: This entire package (and subpackages) is subject to change - do not use this outside of this library.</b> 
+ */
 package com.blackrook.small.struct;
