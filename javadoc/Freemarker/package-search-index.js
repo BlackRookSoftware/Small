0f4cbb546eb94f42215aab9df91206374b2b3a1c
mtrop <mtropiano@gmail.com>
2024-10-20 23:25:18 -0400

    Update for SmallFreemarker 1.0.1

--- javadoc/Freemarker/package-search-index.js
+++ javadoc/Freemarker/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.blackrook.small.views.freemarker"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.blackrook.small.views.freemarker"}];updateSearchResults();
--- javadoc/Freemarker/type-search-index.js
+++ javadoc/Freemarker/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.blackrook.small.views.freemarker","l":"FreemarkerViewDriver"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.blackrook.small.views.freemarker","l":"FreemarkerViewDriver"}];updateSearchResults();
